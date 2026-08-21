36f84fa57da016c1c0a7531da523b404efe68d84
_VISHV_ <vishvpratap466@gmail.com>
2026-08-21 15:05:01 +0530

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.cpp
@@ -0,0 +1,25 @@
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         // naive solution
+//         // in s.size/2 time complexity 
+//         int n = s.size();
+//         int i=0;
+//         int j = n-1;
+//         while(i<j){
+//             if(s[i]!=s[j]) swap(s[i],s[j]);
+//             i++; j--;
+//         }
+//       //  return s;
+//     }
+// };
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            swap(s[l++], s[r--]);
+        }
+    }
+};
